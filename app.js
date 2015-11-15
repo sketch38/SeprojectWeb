017ed45a69b1193e13edf6ba1af30c311dbbb817
Sketch38 <jiratchayas-01@hotmail.com>
2015-11-15 18:28:50 +0700

    change node

--- app.js
+++ app.js
@@ -77,17 +77,18 @@ function handle_database(request,response){
 	pool.getConnection(function(error,conn){
 		var queryString ={};
 		if(path == '/news'){
-			queryString = "SELECT * FROM event E,category Ca,time T WHERE E.eid = T.eid AND Ca.ca_id = E.ca_id";
+			queryString = "SELECT E.eid,E.title,E.detail,T.time_post,Ca.ca_name FROM event E,category Ca,time T WHERE E.eid = T.eid AND Ca.ca_id = E.ca_id ORDER BY T.time_post DESC";
 		}
 		else if(path == '/news/:eid'){
 			var id = request.params.eid;
-			queryString = 'SELECT * FROM `event` WHERE `eid`='+id;
-		}
+			queryString = "SELECT E.eid,E.title,E.detail,T.time_post,Ca.ca_name,TIME_FORMAT(T.start_time, '%H:%i') start_time,TIME_FORMAT(T.end_time, '%H:%i') end_time,T.start_date,T.end_date FROM event E,category Ca,time T WHERE E.eid = T.eid AND Ca.ca_id = E.ca_id AND E.eid ="+id;
+        }
         else if(path == '/category'){
             queryString = 'SELECT ca_name FROM `category`';
         }
         else if(path == '/time-table'){
-            queryString = 'SELECT * FROM time T,course C,category Ca,courseday Cd WHERE C.cid = T.cid and Ca.ca_id = C.ca_id and Cd.cid = C.cid';
+            queryString = "SELECT C.cid,C.cnum,C.title,C.teacher,C.type,Ca.ca_name,Ca.color, TIME_FORMAT(T.start_time, '%H:%i') start_time,TIME_FORMAT(T.end_time, '%H:%i') end_time,T.room,Cd.day FROM time T,course C,category Ca,courseday Cd WHERE C.cid = T.cid and Ca.ca_id = C.ca_id and Cd.cid = C.cid";
+            //event query-> SELECT E.eid,E.title,Ca.ca_name,Ca.color,TIME_FORMAT(T.start_time, '%H:%i') start_time,TIME_FORMAT(T.end_time, '%H:%i') end_time,T.room FROM time T,event E,category Ca WHERE E.eid = T.eid and Ca.ca_id = E.ca_id
         }
 		conn.query(queryString,function(error,results){
 			if(error){
